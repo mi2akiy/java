15c6454d31f8b6bf908650190269b9c5e11cdb68
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-11-30 06:30:50 +0000

    [finish]4-1

--- StuSample.java
+++ StuSample.java
@@ -1,11 +1,13 @@
-class StuSample{
+public class StuSample{
   public static void main(String[] args){
-    Student stu1 = new Student();
-
-    stu1.name = "菅原";
-    stu1.setScore(90,80);
-
-    stu1.display();
-    System.out.println("平均" + stu1.getAvg() + "点");
+    Student stu = new Student();
+    
+    // スーパークラスのメソッド
+    stu.setName("菅原");
+    stu.display();
+    
+    // サブクラスのメソッド
+    stu.setStuNo(1);
+    stu.displayStuNo();
   }
 }
