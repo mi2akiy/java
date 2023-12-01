9321c5004b039488ed4c6e5d1edfe41e24ed4b5e
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-12-01 05:56:36 +0000

    [finish]4-6

--- StuSample5.java
+++ StuSample5.java
@@ -1,12 +1,17 @@
-class StuSample5{
+public class StuSample5{
   public static void main(String[] args){
-    Student5 stu1 = new Student5("菅原");
-    stu1.setScore(80);
-    stu1.display();
-    
-    Student5 stu2 = new Student5("村山");
-    stu2.setScore(-50);
-    // stu2.score = -50;
-    stu2.display();
+    Student5 stu1 = new Student5("菅原", 1);
+    Person5 psn = stu1;
+    psn.display();
+    // psn.shgStuNo(1001);
+
+    /*補足：instanceofキーワード
+      オブジェクトのクラスを特定する
+      対象オブジェクト instanceof クラス名*/
+    if(psn instanceof Student5){
+      Student5 stu2 = (Student5)psn;
+      stu2.chgStuNo(1001);
+      stu2.display();
+    }
   }
 }
